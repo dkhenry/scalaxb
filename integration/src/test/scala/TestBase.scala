b47524311bd85f2b4e996e30dc35b87864b4c2ab
e.e d3si9n <eed3si9n@gmail.com>
2011-01-09 10:16:40 -0500

    top-level element.

--- integration/src/test/scala/TestBase.scala
+++ integration/src/test/scala/TestBase.scala
@@ -1,10 +1,9 @@
 import org.specs2.mutable._
 import java.io.{File}
 import scalaxb.compiler.{Module, Verbose}
-import scalaxb.compiler.xsd.{Driver}
 
 trait TestBase extends Specification with CompilerMatcher {
-  val module: Module = new Driver // with Verbose
+  val module: Module = new scalaxb.compiler.xsd2.Driver // with Verbose
   val tmp = new File("tmp")
   if (tmp.exists) deleteAll(tmp)
   tmp.mkdirs() // you need this for copyFileFromResource
